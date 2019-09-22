d930fc06373e0c279a967cd59758e1b55258490b
Casey <caseyorigami@gmail.com>
2019-09-21 20:54:09 -0700

    Initial commit of prototype

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # auspex
- compact arduino rangefinger for warhammer
+
+Learning arduino by building a compact rangefinger for warhammer. Praise the motive force.
+
+## Controller - Arduino Nano
+
+I'm using [Arduino IDE](https://www.arduino.cc/en/Main/Software) to develop the program.
+I think I have an older version of A Robotdyne clone with a built in USB programmer;
+ - Tools->Board: Arduino Nano
+ - Tools->Processor: ATmega328P (Old Bootloader)
+
+## Display - SSD1306
+
+A monochrome 128 x 64 OLED display which is controlled with I2C.
+
+I'm currently using [Adafruit's GFX and SSD1306 libraries](https://github.com/adafruit/Adafruit_SSD1306) for graphics.
+
+## Sensor - VL53L0X
+
+I was amazed this [sensor](https://www.st.com/en/imaging-and-photonics-solutions/vl53l0x.html) exists.
+    
+I use [Polulu's VL53L0X library](https://github.com/pololu/vl53l0x-arduino) to command it.
+
+## TODO
+
+ - Power
+   - [ ] Determine battery
+   - [ ] figure out convenient charging
+ - Calibration
+   - [ ] Convert to imperial units
+   - [ ] characterized accuraccy up to 48"
+   - [ ] measure cone angle to see if this can even be used in a cluttered environment.
+     - are optics an option if this needs to be corrected?
+ - Targeting laser
+   - I need a visible laser to point the sensor accurately
+   - [ ] Test if it degrades sensor accuracy?
+ - Cabinet
+   - [ ] investigate 'Tiny Duino' for better form factor
+   - [ ] Cut portal and mount screen
+   - [ ] Cut portal and mount sensor
+   - [ ] Cut portal and mount laser
+   - [ ] Mount controller
+   - [ ] Mount battery
+   - [ ] Etch ruler for shorter distance
+
+## Notes
+
+ - To install the needed libraries go to Tools->Manage Libraries and search for the above projects
+ - I cobbled the hardware together from cheap clones bundled from kits and some amazon searches; Going forward I'll buy some from people making the convenient software and kits. Here are some links;
+   - Arduino [nano](https://store.arduino.cc/usa/arduino-nano)
+   - polulu's [VL53L0X](https://www.pololu.com/product/2490) carrier
+   - adafruit's [SDD1306](https://www.adafruit.com/product/326) carrier
---
+++ auspex.ino
@@ -0,0 +1,131 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <VL53L0X.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET 4
+
+VL53L0X sensor;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+unsigned int px = 0;
+int v[SCREEN_WIDTH];
+//int py = 64;
+//int vy = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize the ToF sensor in continuous mode
+  sensor.init();
+  sensor.setTimeout(500);
+  sensor.startContinuous();
+  
+  // clear the array of readings
+  for (int n=0; n<SCREEN_WIDTH; n++)
+    v[n] = 0;
+
+  // seed the rng
+  randomSeed(analogRead(0));
+
+  // initialize the display
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // generate display voltage from 3.3v internally?
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  display.setTextSize(5);
+  display.setTextColor(INVERSE);
+
+  // Adafruit splash
+  display.display();
+  delay(100);
+
+//  display.startscrollright(0x00,0x0f);
+}
+
+void loop() {
+  display.clearDisplay();
+  
+//  // simulate a smoothly varying measurement.
+//  vy += (random(5)-3);
+//  if (vy<-4)
+//    vy=0;//-4;
+//  else if (vy>4)
+//    vy=0;//4;
+//  
+//  py += vy;
+//  if (py<0)
+//    py = 0;
+//  else if (py>=SCREEN_HEIGHT)
+//    py = SCREEN_HEIGHT-1;
+//  v[px] = py;
+
+  v[px] = sensor.readRangeContinuousMillimeters();
+  v[px] /= 10;
+  if (v[px]>64)
+    v[px]=64;
+
+  for (int n=0; n<SCREEN_WIDTH; n++)
+    display.drawFastVLine(n, 0, v[(n+px)%SCREEN_WIDTH], WHITE);
+
+  display.setCursor(15,15);
+  display.print(String(v[px]));
+
+  px = (px+1) % SCREEN_WIDTH;
+  display.display();
+  delay(50);
+}
+
+///* This example shows how to use continuous mode to take
+//range measurements with the VL53L0X. It is based on
+//vl53l0x_ContinuousRanging_Example.c from the VL53L0X API.
+//
+//The range readings are in units of mm. */
+//
+//#include <Wire.h>
+//#include <VL53L0X.h>
+//
+//VL53L0X sensor;
+//
+//void setup()
+//{
+//  Serial.begin(9600);
+//  Wire.begin();
+//
+//  sensor.init();
+//  sensor.setTimeout(500);
+//
+//  // Start continuous back-to-back mode (take readings as
+//  // fast as possible).  To use continuous timed mode
+//  // instead, provide a desired inter-measurement period in
+//  // ms (e.g. sensor.startContinuous(100)).
+//  sensor.startContinuous();
+//}
+//
+//void loop()
+//{
+//  Serial.print(sensor.readRangeContinuousMillimeters());
+//  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+//
+//  Serial.println();
+//}
+
+//// try an approach where we only update the measurement and a single column everyframe...
+//void loop() { // unfortunately I can't really figure out how to update the buffer and scroll it simultaneously...
+//  
+////  px = (px+1) % SCREEN_WIDTH;  
+//  display.drawFastVLine(px, 0, SCREEN_HEIGHT, BLACK);
+//  py = random(0, SCREEN_HEIGHT);
+//  display.drawFastVLine(px, 0, py, WHITE);
+//
+//  // remove old measurement by reapeating text with inverse color
+//  // scroll screen horizontally
+//  // write new measurement?
+//  // optionally we can just see how fast we can draw the whole screen...
+//  
+////  display.display();
+//  delay(50);
+//}
